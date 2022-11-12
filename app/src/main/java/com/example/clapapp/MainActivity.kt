8489e36d732555f7e036c755f677d2fa9b78a23a
mahabub.pasha <mahbub.pasha@randomaccessinc.com>
2022-11-12 11:38:45 +0600

    audio player play,pause and stop buton functionality

--- app/src/main/java/com/example/clapapp/MainActivity.kt
+++ app/src/main/java/com/example/clapapp/MainActivity.kt
@@ -3,18 +3,82 @@ package com.example.clapapp
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.widget.Button
+import android.widget.SeekBar
+import android.widget.TextView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var mediaPlayer: MediaPlayer
+    private lateinit var seekBar: SeekBar
+    private var mediaPlayer: MediaPlayer? = null
+    private lateinit var runnable: Runnable
+    private lateinit var handler: Handler
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        mediaPlayer = MediaPlayer.create(this, R.raw.applauding)
+        seekBar = findViewById(R.id.sbClapping)
+        handler = Handler(Looper.getMainLooper())
+//        mediaPlayer = MediaPlayer.create(this, R.raw.applauding)
 
-//        val button = findViewById<Button>(R.id.btnClap)
-//        button.setOnClickListener {
-//            mediaPlayer.start()
-//        }
+        val playBtn = findViewById<FloatingActionButton>(R.id.fabPlay)
+        playBtn.setOnClickListener {
+            if(mediaPlayer == null){
+                // re-initialize mediaplayer
+                mediaPlayer = MediaPlayer.create(this, R.raw.applauding)
+                initializeSeekBar()
+            }
+            mediaPlayer?.start()
+        }
+
+        val pauseBtn = findViewById<FloatingActionButton>(R.id.fabPause)
+        pauseBtn.setOnClickListener {
+            mediaPlayer?.pause()
+        }
+
+        val stopBtn = findViewById<FloatingActionButton>(R.id.fabStop)
+        stopBtn.setOnClickListener {
+            mediaPlayer?.stop()
+            mediaPlayer?.reset()
+            mediaPlayer?.release()
+            mediaPlayer = null
+            handler.removeCallbacks(runnable)
+            seekBar.progress = 0
+        }
+    }
+    private fun initializeSeekBar(){
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                if(fromUser) mediaPlayer?.seekTo(progress)
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+
+            }
+
+        })
+
+        val tvPlayed = findViewById<TextView>(R.id.tvPlayed)
+        val tvDue = findViewById<TextView>(R.id.tvDue)
+
+        // !! not null assertion
+        seekBar.max = mediaPlayer!!.duration
+        runnable = Runnable {
+            seekBar.progress = mediaPlayer!!.currentPosition
+
+            val playTime = mediaPlayer!!.currentPosition/1000
+            tvPlayed.text = "$playTime sec"
+            val duration = mediaPlayer!!.duration/1000
+            val dueTime = duration - playTime
+            tvDue.text = "$dueTime sec"
+
+            handler.postDelayed(runnable, 1000)
+        }
+        handler.postDelayed(runnable, 1000)
     }
 }
